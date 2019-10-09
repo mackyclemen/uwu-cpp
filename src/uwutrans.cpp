e33353a0f98efb7f77be01714323951dd0e0ab8e
Nagisa-Masahiko <Sitt.Evan@gmail.com>
2019-10-09 11:23:32 +0200

    Adding support for command line arguments.
    Moved original greeting to an if block. uwu away!

--- src/uwutrans.cpp
+++ src/uwutrans.cpp
@@ -2,17 +2,23 @@
 #include <string>
 #include "uwulib.h"
 
-int main() {
+int main(int argc, char** argv) {
 
 	// Initiate a variable to store input
 	std::string stringToTranslate;
-	
-	// Might as well greet the user
-	std::cout << "Welcome to the uwu translator!" << std::endl;
-	std::cout << "Enter string to translate: ";
-	
-	// Gets user input and saves it to a variable
-	std::getline(std::cin, stringToTranslate);
+	if(argc<2)
+	{
+		// Might as well greet the user
+		std::cout << "Welcome to the uwu translator!" << std::endl;
+		std::cout << "Enter string to translate: ";
+		
+		// Gets user input and saves it to a variable
+		std::getline(std::cin, stringToTranslate);
+	}
+	else
+	{
+		stringToTranslate = argv[1];
+	}
 	
 	// runs the translator and prints it out
 	std::cout << uwu::translate(stringToTranslate);
